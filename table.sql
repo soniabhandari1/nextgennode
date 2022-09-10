20a46a07c3d43b2a9575ffd87383196e3fa9aaf2
Sonia Bhandari <soniabhandari19nov@gmail.com>
2022-09-10 13:42:09 +0530

    first commit

---
+++ table.sql
@@ -0,0 +1,12 @@
+create table user(
+    id int primary key Auto_Increment,
+    name varchar(250),
+    contactNumber varchar(20),
+    email varchar(50),
+    password varchar(250),
+    status varchar(20),
+    role varchar(20),
+    UNIQUE (email)
+    );
+
+    insert into user(name,contactNumber,email,password,status,role)values('Admin','9988461939','vinaybhandari@nextgenapps.in','123456','true','admin');
