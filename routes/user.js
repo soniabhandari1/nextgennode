29df433402b59798243e0642bd9f668fbdcfd256
Sonia Bhandari <soniabhandari19nov@gmail.com>
2022-09-10 19:01:46 +0530

    formatted

--- routes/user.js
+++ routes/user.js
@@ -1,36 +1,36 @@
-const express=require('express');
-const connection=require('../connection')
-const router=express.Router()
+const express = require("express");
+const connection = require("../connection");
+const router = express.Router();
 
-router.post('/signup',(req,res)=>{
-    let user=req.body;
-    
-    let query='select email,password,role,status from user where email=?';
-    connection.query(query,[user.email],(err,results)=>{
-        if(!err){
-            if(results.length<=0){
-                let query="insert into user(name,contactNumber,email,password,status,role) values(?,?,?,?,'false','user')";
-                connection.query(query,[user.name,user.contactNumber,user.email,user.password],(err,results)=>{
-                    if(!err){
-                        
-                        return res.status(200).json({message:'Successfully Registered'})
-                    }
-                    else{
-                        return res.status(500).json(err)
-                    }
-                })
+router.post("/signup", (req, res) => {
+  let user = req.body;
+
+  let query = "select email,password,role,status from user where email=?";
+  connection.query(query, [user.email], (err, results) => {
+    if (!err) {
+      if (results.length <= 0) {
+        let query =
+          "insert into user(name,contactNumber,email,password,status,role) values(?,?,?,?,'false','user')";
+        connection.query(
+          query,
+          [user.name, user.contactNumber, user.email, user.password],
+          (err, results) => {
+            if (!err) {
+              return res
+                .status(200)
+                .json({ message: "Successfully Registered" });
+            } else {
+              return res.status(500).json(err);
             }
-            else{
-                return res.status(400).json({message: 'Email Already Exist'})
-            }
-
+          }
+        );
+      } else {
+        return res.status(400).json({ message: "Email Already Exist" });
+      }
+    } else {
+      return res.status(500).json(err);
     }
-    else{
-        return res.status(500).json(err)
-    }
-})
-    
-})
+  });
+});
 
-
-module.exports=router;
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-require('dotenv').config()
-const http=require('http')
-const app=require('./index')
-const server=http.createServer(app)
-server.listen(process.env.PORT,()=>{console.log(`server is listening on ${process.env.PORT}`)})
+require("dotenv").config();
+const http = require("http");
+const app = require("./index");
+const server = http.createServer(app);
+server.listen(process.env.PORT, () => {
+  console.log(`server is listening on ${process.env.PORT}`);
+});
